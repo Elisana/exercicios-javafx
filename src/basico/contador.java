4ec3e2c49becd7ee157de8c2ee79266d331b6175
Elisana <elisana.favarin@gmail.com>
2021-08-19 18:36:38 -0300

    Seção 18 - JavaFx

--- src/basico/contador.java
+++ src/basico/contador.java
@@ -8,7 +8,7 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
+ 
 public class contador extends Application {
 
 	private int contador = 0;
